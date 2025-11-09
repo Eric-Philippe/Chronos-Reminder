f21b10725bf2d93f98f854dba4946d58abe17b11
Eric-Philippe <ericphlpp@gmail.com>
2025-11-09 12:22:40 +0100

    Add Status

--- internal/bot/events/ready.go
+++ internal/bot/events/ready.go
@@ -8,4 +8,9 @@ import (
 
 func Ready(s *discordgo.Session, event *discordgo.Ready) {
 	log.Printf("[DISCORD_BOT] - 🤖 Bot is ready! Logged in as: %s#%s", event.User.Username, event.User.Discriminator)
+
+	err := s.UpdateListeningStatus("⏳ https://chronosrmd.com ⌛️")
+	if err != nil {
+		log.Printf("[DISCORD_BOT] - ⚠️ Error setting bot status: %v", err)
+	}
 }
